59b34e77ca950f5dfbbfc53ad577d12287d9ba16
Taylor <taylorhcarroll1@gmail.com>
2020-05-28 17:44:08 -0700

    but why more models?

---
+++ RideAwareFS/RideAwareFS/Models/StudentUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideAwareFS.Models
+{
+    public class StudentUser
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int UserId { get; set; }
+        public bool PrimaryUser { get; set; }
+        public bool Expire { get; set; }
+        public DateTime ExpireDate { get; set; }
+
+
+    }
+}
---
+++ RideAwareFS/RideAwareFS/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RideAwareFS.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public bool Admin { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        public int LocationId { get; set; }
+    }
+}
